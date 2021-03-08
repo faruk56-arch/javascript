31a6c2398f157464459814c32a547f4184f8e8a9
faruk67S <faruk67S@users.noreply.github.com>
2021-03-08 15:41:50 +0100

    js

---
+++ jour-1/jour-1.js
@@ -0,0 +1,38 @@
+//console.log("hello world");//
+//const test = "My name is faruk";//
+
+/*const name = "faruk";
+console.log("Nice to meet you" + " " + name)*/
+
+
+/*const testLength = "I'm very long !"
+console.log(testLength.length)*/
+
+
+/*const food = "croissant is meh"
+
+console.log(food.replace('meh', "so good"));*/
+
+/*const basic = "This is cool"
+const basicUp = basic.toUpperCase();
+const basicDown = basic.toLowerCase();
+console.log(basic)
+console.log(basicUp)
+console.log(basicDown)*/
+
+/*const word = "banana"
+const letters = word.split('')
+console.log(letters);*/
+
+
+/*const age = "25"
+const template = "I am " + age + " years old " 
+console.log(template);*/
+
+
+/*const changed = "Bonjour"
+ 
+const nChanged = changed.replace(/o/g,"a");
+
+console.log(nChanged);
+*/
