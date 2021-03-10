4faece4a549c6c7beb1e52b06e14460b903dd4f6
faruk67S <faruk67S@users.noreply.github.com>
2021-03-10 17:21:51 +0100

    jr3

---
+++ jour-3/bonus1.js
@@ -0,0 +1,23 @@
+var total = 0;
+const limit = 100;
+
+for(var i = 0; i <= limit; i++) {
+
+    console.log(i);
+}
+
+var i = 15;
+
+if(i = 3) {
+    console.log("fizz");
+} else if(i => 5) {
+    console.log("buzz");
+} else if(i => 15) {
+    console.log("fizzbu");
+} else if(i => 7) {
+    console.log();
+} else {
+    console.log(i);
+}
+
+  
