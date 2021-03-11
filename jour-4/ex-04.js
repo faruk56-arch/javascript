5ccb0d360720e01a5c3dfc726c794f2fe96e6336
faruk67S <faruk67S@users.noreply.github.com>
2021-03-11 17:03:15 +0100

    juor4  2

--- jour-4/ex-04.js
+++ jour-4/ex-04.js
@@ -1,13 +1,17 @@
 
 
 function compare(num1, num2) {
-    if(num1 >= 20) {
+    console.log(num1);
+    console.log(num2);
+
+    if (num1 > num2) {
         console.log("num1 is bigger");
-    } else if(num2 >= 30) {
+
+    } else if (num2 > num1) {
         console.log("num2 is bigger");
-    } else if (num1 === num2) {
+    } else{
         console.log("both are égaux");
     }
 }
 
-compare(44);
+compare(10, 10);
---
+++ jour-4/ex-05.js
@@ -0,0 +1,11 @@
+
+
+
+function addUp(num) {
+    
+    while (num === 1) {
+        num++;
+    }
+}
+
+addUp(12);
