f9ac7a420fda1c9f9aefcda02d00f91dfe019e45
momin <faruk373@gmail.com>
2021-03-26 17:31:55 +0100

    supprem

--- jour-7/exo5.js
+++ jour-7/exo5.js
@@ -4,8 +4,9 @@
 var numbers = [1,2,3,4,5,6,7,8];
 
 
-var even = numbers.filter(function(pair) {
-    return !(pair % 2);
-})
+var even = numbers.filter((pair) => !(pair % 2))
+
+var odd = numbers.filter((element) => element % 2)
 
 console.log(even);
+console.log(odd);
