c803328840954bc912ec1de40d2b970486dca408
momin <faruk373@gmail.com>
2021-03-26 16:56:17 +0100

    projets

--- rover/projets.js
+++ rover/projets.js
@@ -13,28 +13,33 @@ var grid = [
 
 
 const rover = {
-    direction: "1",
-     x: 0,
-     y: 0
+    direction: "N",
+    x: 0,
+    y: 0,
+    travelLog:[]
 }
-
+//fonction erreur 
+function ere(){
+    return "erreur"
+}
 
 
 // funtion turnLeft
 function turnLeft(rover) {
-    if(rover.direction === "N") {
+    if (rover.direction === "N") {
         rover.direction = "E"
         return rover;
-    } else if(rover.direction === "E") {
+    } else if (rover.direction === "E") {
         rover.direction = "S"
         return rover;
-
-    } else if(rover.direction === "S") {
+    } else if (rover.direction === "S") {
         rover.direction = "W"
         return rover;
-    } else {
+    } else if (rover.direction === "W") {
         rover.direction = "N"
         return rover;
+    } else {
+        return;
     }
 
 }
@@ -42,45 +47,79 @@ function turnLeft(rover) {
 
 
 // function tornRight
-function turnRight(rover) { 
-    
-    if(rover.direction === "N") {
+function turnRight(rover) {
+
+    if (rover.direction === "N") {
         rover.direction = "W"
-        return rover.direction;
-    } else if(rover.direction === "W") {
+        return rover;
+    } else if (rover.direction === "W") {
         rover.direction = "S"
-        return rover.direction;
+        return rover;
 
-    } else if(rover.direction === "S") {
+    } else if (rover.direction === "S") {
         rover.direction = "E"
-        return rover.direction;
-    } else {
+        return rover;
+    } else if (rover.direction === "E") {
         rover.direction = "N"
-        return rover.direction;
+        return rover;
+    } else {
+        return;
     }
-    
+
 }
-console.log(turnRight(rover));
+// console.log(turnRight(rover));
 // console.log(rover);
-
-
- 
-
-
 
 // movement du rover
 
 function moveForward(rover) {
-if(rover === "N" && turnRight === "W")  {
-    rover.direction = "W";
-    console.log(rover); 
-    rover.x = 1;
-    console.log(rover);
-    // return rover;
-    
+
+    if (rover.direction === "E") {
+        rover.x += 1;
+        return rover;
+    }
+    else if (rover.direction === "W") {
+        rover.x -= 1;
+        return rover;
+    }
+    else if (rover.direction === "S") {
+        rover.y += 1;
+        return rover;
+    }
+    else if (rover.direction === "N") {
+        rover.y -= 1;
+        return rover;
+    } else {
+        return;
+    }
 
 }
 
+// console.log(moveForward(rover));
 
-}
-moveForward(rover);
+function pilotRover(str) {
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === "l") {
+            turnLeft(rover);
+            rover.travelLog.push([rover.x ,rover.y])
+        }
+        else if (str[i] === "r") {
+            turnRight(rover);
+            rover.travelLog.push([rover.x ,rover.y])
+
+        }
+        else if (str[i] === "f") {
+            moveForward(rover)
+            rover.travelLog.push([rover.x ,rover.y])
+
+        }
+        else {
+               return  "erreur"  
+            }
+
+        }
+    }
+
+
+pilotRover("lrfflf")
+console.log(rover)
